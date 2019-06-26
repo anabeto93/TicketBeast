421152a869c71155919e2b051d144b1762414b82
Richard Anabeto Opoku <anabeto93@gmail.com>
2019-06-26 21:53:30 +0000

    reversed the relationship between concerts and orders while also ensuring nothing is broken in the process. Removed the redundant relationship between orders and concerts since tickets can serve as a pivot table

--- app/Models/Concert.php
+++ app/Models/Concert.php
@@ -32,7 +32,7 @@ class Concert extends Model
 
     public function orders()
     {
-        return $this->hasMany(Order::class);
+        return $this->belongsToMany (Order::class,'tickets');
     }
 
     public function tickets()
@@ -62,7 +62,7 @@ class Concert extends Model
 
     public function createOrder($email, $tickets)
     {
-        $order = $this->orders()->create([
+        $order = Order::create([
             'email' => $email,
             'amount' => $tickets->count() * $this->ticket_price,
         ]);

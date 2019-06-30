e818dcc96463fd3650f1fd89ec1bd5a27b5f552a
Richard Anabeto Opoku <anabeto93@gmail.com>
2019-06-30 12:23:50 +0000

    moved the tests into contracts

--- tests/Unit/Billing/FakePaymentGatewayTest.php
+++ tests/Unit/Billing/FakePaymentGatewayTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Unit\Billing;
 
 use App\Models\Concert;
 use App\Billing\FakePaymentGateway;
@@ -10,66 +10,11 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class FakePaymentTest extends TestCase
 {
-    use RefreshDatabase;
+    use RefreshDatabase, PaymentGatewayContractTest;
     
     protected function getPaymentGateway()
     {
         return new FakePaymentGateway();
-    }
-
-    /**
-     * @test
-     */
-    function can_fetch_charges_created_during_a_callback()
-    {
-        $paymentGateway = $this->getPaymentGateway();
-        $paymentGateway->charge(1000, $paymentGateway->getValidTestToken());
-        $paymentGateway->charge(1500, $paymentGateway->getValidTestToken());
-        $paymentGateway->charge(2500, $paymentGateway->getValidTestToken());
-
-        $newCharges = $paymentGateway->newChargesDuring(function ($paymentGateway) {
-            $paymentGateway->charge(3000, $paymentGateway->getValidTestToken());
-            $paymentGateway->charge(5000, $paymentGateway->getValidTestToken());
-        });
-
-        $this->assertCount(2, $newCharges);
-        $this->assertEquals([3000, 5000], $newCharges->all());
-    }
-
-    /**
-     * @test  
-     */
-    function charges_with_a_valid_payment_token_are_successful()
-    {
-        $paymentGateway = $this->getPaymentGateway();
-
-        $charge = $paymentGateway->lastCharge();
-
-        $newCharges = $paymentGateway->newChargesDuring(function($paymentGateway) {
-            $paymentGateway->charge(2500, $paymentGateway->getValidTestToken());
-        });
-
-        $this->assertCount(1, $newCharges);
-        $this->assertEquals(2500, $newCharges->sum());
-    }
-
-    /**
-     * @test
-     */
-    function charges_with_invalid_payment_token_fails()
-    {
-        try{
-            $paymentGateway = $this->getPaymentGateway();
-
-            $paymentGateway->charge(2500, 'hahaha-faked-token');
-        }catch(\App\Billing\PaymentFailedException $exception) {
-
-            $this->assertTrue(true);
-
-            return;
-        }
-
-        $this->fail();
     }
 
     /**
--- tests/Unit/Billing/StripePaymentGatewayTest.php
+++ tests/Unit/Billing/StripePaymentGatewayTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Unit\Billing;
 
 use App\Billing\StripePaymentGateway;
 use App\Models\Concert;
@@ -16,7 +16,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
  */
 class StripePaymentGatewayTest extends TestCase
 {
-    use RefreshDatabase;
+    use RefreshDatabase, PaymentGatewayContractTest;
 
     public function setUp(): void
     {
@@ -26,60 +26,5 @@ class StripePaymentGatewayTest extends TestCase
     protected function getPaymentGateway()
     {
         return new StripePaymentGateway();
-    }
-
-    /**
-     * @test
-     */
-    function can_fetch_charges_created_during_a_callback()
-    {
-        $paymentGateway = $this->getPaymentGateway();
-        $paymentGateway->charge(1000, $paymentGateway->getValidTestToken());
-        $paymentGateway->charge(1500, $paymentGateway->getValidTestToken());
-        $paymentGateway->charge(2500, $paymentGateway->getValidTestToken());
-
-        $newCharges = $paymentGateway->newChargesDuring(function ($paymentGateway) {
-            $paymentGateway->charge(3000, $paymentGateway->getValidTestToken());
-            $paymentGateway->charge(5000, $paymentGateway->getValidTestToken());
-        });
-
-        $this->assertCount(2, $newCharges);
-        $this->assertEquals([3000, 5000], $newCharges->all());
-    }
-
-    /**
-     * @test
-     */
-    function charges_with_a_valid_payment_token_are_successful()
-    {
-        $paymentGateway = $this->getPaymentGateway();
-
-        $charge = $paymentGateway->lastCharge();
-
-        $newCharges = $paymentGateway->newChargesDuring(function($paymentGateway) {
-            $paymentGateway->charge(2500, $paymentGateway->getValidTestToken());
-        });
-
-        $this->assertCount(1, $newCharges);
-        $this->assertEquals(2500, $newCharges->sum());
-    }
-
-    /**
-     * @test
-     */
-    function charges_with_invalid_stripe_token_fails()
-    {
-        try{
-            $paymentGateway = $this->getPaymentGateway();
-
-            $paymentGateway->charge(2500, 'hahaha-faked-token');
-        }catch(\App\Billing\PaymentFailedException $exception) {
-
-            $this->assertTrue(true);
-
-            return;
-        }
-
-        $this->fail('Charging with an invalid stripe token not throwing the PaymentFailedException');
     }
 }
